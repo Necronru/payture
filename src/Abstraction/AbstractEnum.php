4d8c00d3bd876e92f0aa8cd33cd985c8f1d4b94e
Roman Toroshin <necronru@gmail.com>
2017-03-20 19:49:37 +0300

    added AbstractEnum

---
+++ src/Abstraction/AbstractEnum.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Necronru\Payture\Abstraction;
+
+abstract class AbstractEnum
+{
+    /**
+     * Хеш с названиями элементов перечисления на русском языке
+     * Не обязателен для перегрузки
+     * @var array
+     */
+    protected static $titles = [];
+
+    /**
+     * Возвращает хэш вида key => value,
+     * где key - название константы в перечислении,
+     * а value - значение константы в перечислении
+     * @return array
+     */
+    public static function getItems()
+    {
+        return (new \ReflectionClass(static::class))->getConstants();
+    }
+
+    /**
+     * Возвращает хэш вида key => value,
+     * где key - значение константы в перечислении,
+     * а value - русское название константы в перечислении
+     * @return array
+     */
+    public static function getTitles()
+    {
+        return static::$titles;
+    }
+
+    public static function contains($value)
+    {
+        return in_array($value, static::getItems());
+    }
+}
